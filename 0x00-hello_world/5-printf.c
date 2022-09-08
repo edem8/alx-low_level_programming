53a2e63362dc8b5a186532ec8c0a81772938e098
edem <eddieblack159@gmail.com>
2022-09-08 23:49:49 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints using printf
+ * main - prints using printf, 
+ * without using puts
  *
  * Return: return (0) or nothing
 */
 int main(void)
 {	
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
