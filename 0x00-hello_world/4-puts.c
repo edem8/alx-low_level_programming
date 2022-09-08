ab46e2f49c9430f126deba4b8b99a5a6e769a4ca
edem <eddieblack159@gmail.com>
2022-09-08 22:51:32 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * puts - prints out a string
+ *
+ * Return: Programming is like building a multilingual
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel");
+	return (0);
+}
