d7dd404a1c2d9f9d6a22c0f799b64173c6882370
edem <eddieblack159@gmail.com>
2022-09-08 23:14:05 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-prints using printf
+ *
+ * Return:return (0) or nothing
+*/
+
+int main()
+{	
+	char str1[22]="with proper grammer,";
+	char str2[36]="but the outcome is a piece of art,";
+	printf("%s %s.\n",str1,str2);
+}
