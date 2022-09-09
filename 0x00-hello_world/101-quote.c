33085b4d941cc086774c1e5ecc9b7d8409ed20c6
edem <eddieblack159@gmail.com>
2022-09-09 01:50:45 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - printing strings to standard error
+ *
+ *Return: return (1) or nothing
+*/
+
+int main(void)
+{fprintf(stderr,"and that piece of art is useful\" -Dora korpar, 2015-10-19\n");
+return (1);
+}
