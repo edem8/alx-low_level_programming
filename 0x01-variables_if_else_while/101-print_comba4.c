f960a86391dca9d4dda1b4f19b54267cf16ae350
edem <eddieblack159@gmail.com>
2022-09-11 19:30:02 +0000

    101-print_comba4.c

---
+++ 0x01-variables_if_else_while/101-print_comba4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - printing three digit combination
+ *
+ * Return: return (0) or nothing
+*/
+int main(void)
+{
+        int i = 48;
+
+        for (; i < 56 ; i++)
+        {
+                int a = 1;
+
+                for (; a < 57 ; a++)
+                {
+			int c = 2;
+
+			for (; c <58 ; c++)
+			{
+				if (i < a && a < c)
+				{
+					putchar(i);
+					putchar(a);
+					putchar(c);
+					if (i != 55 || a != 56 || c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+        putchar('\n');
+        return (0);
+}
+
+
