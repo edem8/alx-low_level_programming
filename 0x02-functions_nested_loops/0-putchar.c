6e6b6668d3697d9fd7f9b929842550a372a41582
edem <eddieblack159@gmail.com>
2022-09-13 21:48:14 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - printing putchar
+ *
+ *Return: return (0)
+*/
+int main(void)
+{
+		char str[10];
+
+		strcpy(str,"_putchar");
+		printf("%s\n", str);
+		return (0);
+}
