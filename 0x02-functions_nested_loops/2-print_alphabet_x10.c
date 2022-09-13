362663b9b172325e13f35aea69ed33fd8c36bd3d
edem <eddieblack159@gmail.com>
2022-09-13 23:37:55 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - printing ten times
+ *
+ * Return: return nothing
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	for (; i <= 10; i++)
+	{
+		char a = 97;
+		
+		while (a < 123)
+		{
+			putchar(a);
+			a++;
+		}
+		putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/main_2.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_2_H
+#define MAIN_2_H
+
+void print_alphabet_x10(void);
+
+#endif
