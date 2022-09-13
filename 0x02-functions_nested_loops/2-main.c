5b4c8255acab60bc8dc799e8635106e7eb2689be
edem <eddieblack159@gmail.com>
2022-09-13 23:45:41 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main_2.h"
+#include "main.h"
 
 /**
  * main - printing ten times
--- 0x02-functions_nested_loops/main_2.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_2_H
-#define MAIN_2_H
-
-void print_alphabet_x10(void);
-
-#endif
