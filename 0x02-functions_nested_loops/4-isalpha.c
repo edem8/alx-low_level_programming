84b08530350b588789d6ea28f9df978c46fb8df1
edem <eddieblack159@gmail.com>
2022-09-14 21:35:11 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,9 @@
 #include <ctype.h>
 
 /**
- * main - printing if alphabet
+ * _isalpha - printing if alphabet
  * @c: Parameter to pass
- *
+ *  
  * Return: return (1) or (0)
 */
 int _isalpha(int c)
