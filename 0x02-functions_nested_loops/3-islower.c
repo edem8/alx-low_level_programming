03cb7b13c0ec6e5b1f935c10e31b07f370380479
edem <eddieblack159@gmail.com>
2022-09-14 01:55:56 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _islower - printing (0) or (1)
+ * @c: parameter needed for execution
+ *Return: return (0) 0r (1)
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+		
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mian - printing one or zero
+ *
+ * Return: return (1) or (0)
+*/
+int main (void)
+{
+	int r;
+
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
