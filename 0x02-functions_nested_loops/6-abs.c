26476afc7e5e9f0af27f56b36b834f64ca67e199
edem <eddieblack159@gmail.com>
2022-09-14 22:53:22 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 
+ * _abs - printing absolute values
+ * @c: parameter to be used
+ * Return: return (0) or nothing
+*/
+int _abs(int c)
+{
+	return (abs(c));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
 int _islower(int c);
 int _putchar(int a);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int c);
 #endif
