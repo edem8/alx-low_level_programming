86965dea716ca0f2121d47248c1edd628f8176ac
edem <eddieblack159@gmail.com>
2022-09-15 00:43:51 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,12 +19,12 @@ void times_table(void)
 			{
 				_putchar(c + '0');
 			}
-			else if (c < 10 && b != 0)
+			if (c < 10 && b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				-putchar(c + '0');
+				_putchar(c + '0');
 			}
 			else if (c >= 10)
 			{
