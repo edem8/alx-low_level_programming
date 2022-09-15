e7b1af0fdb0973913b5b6a3f35e331adfadb9a55
edem <eddieblack159@gmail.com>
2022-09-15 00:42:08 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,11 +14,12 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			c = a * b;
+
 			if (b == 0)
 			{
 				_putchar(c + '0');
 			}
-			if (c < 10 && b != 0)
+			else if (c < 10 && b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
