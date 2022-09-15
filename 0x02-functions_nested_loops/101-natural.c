a5b98459ed113bd74a123b0cdb27989abd87c908
edem <eddieblack159@gmail.com>
2022-09-15 02:13:39 +0000

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - printing natural numbers
+ *
+ * Return: return nothing
+*/
+int main(void)
+{
+	unsigned long int num3, num5, num;
+	int a = 0;
+	num3 = 0;
+	num5 = 0;
+	num = 0;
+
+	for (; a < 1024; a++)
+	{
+		if ((a % 3) == 0)
+		{
+			num3 = num3 + a;
+		}
+		else if ((a % 5) == 0)
+		{
+			num5 = num5 + a;
+		}
+	}
+	num = num3 + num5;
+	printf("%lu\n", num);
+	return (0);
+}
