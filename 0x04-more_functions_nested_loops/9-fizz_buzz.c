1e8e9a36409359cd386fab4acee470dd27114f7f
edem <eddieblack159@gmail.com>
2022-09-16 02:14:26 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,18 @@ int main(void)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf(" FizBuzz");
+			printf("FizBuzz ");
 		}
 		else if ((i % 3) == 0 && (i % 5) != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 		else if ((i % 5) == 0 && (i % 3) != 0)
-			printf(" Buzz");
+			printf("Buzz ");
 		else if (i == 1)
-			printf(" %d", i);
+			printf("%d ", i);
 		else
-			printf(" %d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
