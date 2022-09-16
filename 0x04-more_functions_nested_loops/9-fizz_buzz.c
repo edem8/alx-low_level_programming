4fc659fcf472e01e3a5ad007994b73d7499e9e9d
edem <eddieblack159@gmail.com>
2022-09-16 01:55:37 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - printing and replacing with Fizz Buzz or FizzBuzz
+ *
+ * Return: return (0) or nothing
+ */
+int main(void)
+{
+	int i = 1;
+
+	for (; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("%s ", "FizBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if ((i % 5) == 0)
+			printf("%s ", "Buzz");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
