3076f71ca5d5ad3564418a4ec058278a401635ff
edem <eddieblack159@gmail.com>
2022-09-19 23:17:24 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: parameter to pass
+ * Return: return nothing
+ */
+void _puts(char *str)
+{
+	puts("%s\n", str);
+}
