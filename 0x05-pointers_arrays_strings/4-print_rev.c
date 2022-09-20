06d361f68efb5567e653582e92c88b3e851f4f90
edem <eddieblack159@gmail.com>
2022-09-20 02:26:14 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,7 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	
-	while (*(s + i) != '\0')
-	{
-		i += 1;
-	}
-	i -= 1;
-	while (i >= 0)
-	{
-		_putchar(*(s + i));
-		i--;
-	}
-	_putchar('\n');
+	char start;
+	int len = 0;
+
 }
