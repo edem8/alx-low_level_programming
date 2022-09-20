a801ed5b47dd7141f8a7501e92399124d8e05ee7
edem <eddieblack159@gmail.com>
2022-09-20 23:03:04 +0000

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <time.h>
+
+/**
+ * main - random passwords
+ *
+ * Return: return (0)
+ */
+int main(void)
+{
+	int s[];
+	int i = 0;
+	int j = 0;
+	int n;
+
+	srand(time(NULL));
+
+	for (; i < 100 ; i++)
+	{
+		s[i] = rand() % 78;
+		j += (s[i] + '0');
+		putchar(s[i] + '0');
+		if ((2772 - j) - '0' < 78)
+		{
+			n = 2772 - j - '0';
+			j += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
