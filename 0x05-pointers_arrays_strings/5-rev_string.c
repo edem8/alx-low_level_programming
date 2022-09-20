bb6a4e9236caa8a454f973b0ba5232299050e71e
edem <eddieblack159@gmail.com>
2022-09-20 14:38:41 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,4 +3,16 @@
 /**
  * rev_string - reversing a string
  * @s: parameter to pass
- * Return: return 
+ * Return: return nothing
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s) - 1;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
