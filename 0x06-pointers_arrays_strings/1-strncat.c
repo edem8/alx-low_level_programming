7bb0fe0232bb901b0dd02e0840674100317a14fb
edem <eddieblack159@gmail.com>
2022-09-21 12:40:22 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strncat - concatenates only some number of strings
  * @dest: parameter to pass
  * @src: parameter to pass
- * n: number of characters to concatenate
+ * @n: number of characters to concatenate
  * Return: return a pointer
  */
 char *_strncat(char *dest, char *src, int n)
