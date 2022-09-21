c335594d0451713ab66cffee9810f85e74baf635
edem <eddieblack159@gmail.com>
2022-09-21 12:26:30 +0000

    0-strcat.c

---
+++ 0x06-pointer_array_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenating two string arrays
+ * @dest: parameter to pass
+ * @src: parameter to pass
+ * Return: return a pointer or array
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
