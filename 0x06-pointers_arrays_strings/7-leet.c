841885cbe9f67950e61964fcda0efd4e75ac850e
edem <eddieblack159@gmail.com>
2022-09-22 22:19:09 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * leet - Encoding a string to 1337
+ * @a: parameter to pass
+ * Return: return (0)
+ */
+char *leet(char *a)
+{
+	int len = strlen(s);
+	int i = 0;
+	char b[] = {aAeEoOtTlL};
+	char c[] = {4433007711};
+
+	for (; i < len ; i++)
+	{
+		if (a[i] == b[i])
+			a[i] = c[i];
+	}
+}	
