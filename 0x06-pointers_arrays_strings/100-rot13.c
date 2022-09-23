73799b7a17b6772e4e4f426f6a0eac0df08ab140
edem <eddieblack159@gmail.com>
2022-09-23 00:12:59 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes as rot13
+ * @c: parameter to pass
+ * Return: return (0);
+ */
+char *rot13(char *c)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
