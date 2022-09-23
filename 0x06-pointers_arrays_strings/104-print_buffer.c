45403ecd4c681736dd1a40809b51a9ba4223794f
edem <eddieblack159@gmail.com>
2022-09-23 01:51:23 +0000

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -32,6 +32,8 @@ void print_buffer(char *b, int size)
 				if (l >= size)
 					break;
 				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
 					printf("%c", *(b + l));
 			}
 			printf("\n");
