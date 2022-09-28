e763d67d59ef54e3a685196dc59fdfdfe8377286
edem <eddieblack159@gmail.com>
2022-09-28 15:36:56 +0000

    2-strlen-recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - printing string in reverse
+ * @s: string pointer
+ * Return: return nothing
+ */
+void _print_rev_recursion(char *s)
+{
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@
 #include <string.h>
 
 void _puts_recursion(char *s);
-
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 #endif
