c88bf28c996bdec1e740e508b149067a2413df01
edem <eddieblack159@gmail.com>
2022-09-28 17:01:49 +0000

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,5 +8,8 @@
  */
 int _pow_recursion(int x, int y)
 {
-	return (pow(x,y));
+	if (y < 0)
+		return (-1);
+	else if (y >= 0)
+		return ((int)pow(x, y));
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: parameter to pass
+ * Return: return sqrt or (-1)
+ */
+int _sqrt_recursion(int n)
+{
+
