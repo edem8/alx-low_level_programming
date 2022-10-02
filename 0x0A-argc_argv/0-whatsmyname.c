818936193fe7f4635402c0fc7f9135b706571670
edem <eddieblack159@gmail.com>
2022-10-02 15:44:18 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the name of a program
+ * @argc: number of arguments
+ * @agrv: array of arguments
+ * Return: return (0)
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
