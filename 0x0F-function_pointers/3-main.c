66a993c1dd87c00215a31b1905205f7c7e126499
edem <eddieblack159@gmail.com>
2022-10-14 23:37:34 +0000

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,6 +16,12 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	o = get_op_func(argv[2]);
 
 	if (o == NULL)
