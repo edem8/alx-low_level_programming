c716cc6fca4dd33ebd418141d7088efe7973482c
edem <eddieblack159@gmail.com>
2022-10-14 23:11:21 +0000

    3-main.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,7 +9,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		printf("Error\n");
+		return;
 	else
 		f(name);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,25 +9,25 @@
 int main(int argc, char **argv)
 {
 	int a, b;
+	int (*o)(int, int);
 
-	if (argc < 4)
+	if (argc < 4 || argc > 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
-	if (argv[2][1] != '\0')
+	
+	o = get_op_func(argv[2]);
+
+	if (o == NULL)
 	{
 		printf("Error\n");
 		exit (99);
 	}
-	if (get_op_func(argv[2]) == NULL)
-	{
-		printf("Error\n");
-		exit (100);
-	}
 	
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	printf("%d\n", (*get_op_func(argv[2]))(a, b));
+
+	printf("%d\n", o(a, b));
 	return (0);
 }
