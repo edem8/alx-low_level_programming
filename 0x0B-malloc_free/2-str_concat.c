ad2e295eea492225ce69ac67985d3512b94daece
edem <eddieblack159@gmail.com>
2022-10-18 03:10:07 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,14 +9,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *arr;
+
 	if (s1 == NULL)
 		s1 = " ";
 	if (s2 == NULL)
 		s2 = " ";
 
-	arr = malloc(sizeof(s1) * strlen(s1) + sizeof(s2) * strlen(s2) + 1);
+	arr = (char*)malloc((sizeof(s1) * strlen(s1)) + (sizeof(s2) * strlen(s2)) + 1);
 	if (arr == NULL)
 		return (NULL);
-	arr = strcat(s1, s2);
 	return (arr);
 }
