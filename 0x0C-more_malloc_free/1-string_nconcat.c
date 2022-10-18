f4b0d3ac70d1eeecce1245969321937b6d7b4072
edem <eddieblack159@gmail.com>
2022-10-18 09:13:55 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,13 +13,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *arr;
 
 	if (s1 == NULL)
-		s1 == " ";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 == NULL;
+		s2 = NULL;
 	if (s1 == NULL && s2 == NULL)
 	{
-		s1 == " ";
-		s2 == " ";
+		s1 = " ";
+		s2 = " ";
 	}
 
 	arr = malloc(len + 1);
