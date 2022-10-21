217ca1c42191572a4a94c629bc92a0f2b703f5ef
edem <eddieblack159@gmail.com>
2022-10-21 11:12:40 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ struct dog
 	 char *owner;
 };
 
-/* declaration of init_dog */
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
