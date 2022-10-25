fb735d89ad803b9567358ba87a9383c6226d2d3e
edem <eddieblack159@gmail.com>
2022-10-25 22:57:21 +0000

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * before_main - Print statement within this fxn before running main fxn.
  */
-void__attribute__((constructor)) before_main(void)
+void __attribute__((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
