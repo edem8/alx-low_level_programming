7def5be79efafec548f5e79bb90eaff2f6a6812b
edem <eddieblack159@gmail.com>
2022-10-31 02:30:39 +0000

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define IKNOW '\0'
+#define MAYBEIKNOW head
+#define IMPOSIBLEKNOW next
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -27,16 +31,7 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-
-/**
- * insert_nodeint_at_index - insert node at given index of linked list
- * @head: double header head
- * @index: where to insert node
- * @n: value to store in node
- * Return: new node or null if failed
- */
-listint_t *insert_nodeint_at_index(listint_t **head,
-				   unsigned int index, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
