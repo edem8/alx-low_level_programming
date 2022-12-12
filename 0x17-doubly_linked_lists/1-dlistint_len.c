9a7a5d03d0e9a349aae5c4fd64a49059859d4ae5
edem <eddieblack159@gmail.com>
2022-12-12 23:01:10 +0000

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in list
+ * @h: pointer to the head of the list
+ * Return: return number of elements in lists
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+	{
+		dprintf(2, "Error: can't  malloc\n");
+		return (EXIT_FAILURE);
+	}
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
